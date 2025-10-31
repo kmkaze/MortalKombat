d7bd5402b497b55f52369e5909b7a4e30a6e6b2a
kmkaze <kmilo.guitar27@gmail.com>
2025-10-31 11:44:07 -0500

    Cambios home y proyecto gnr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Games } from './games/games';
-import { Movies } from './movies/movies';
-import { History } from './history/history';
-import { Characters } from './characters/characters';
+import { Home } from './pages/home/home';
+import { Games } from './pages/games/games';
+import { Movies } from './pages/movies/movies';
+import { History } from './pages/history/history';
+import { Characters } from './pages/characters/characters';
 
 export const routes: Routes = [
   { path: '', component: Home },
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Header } from './componentes/header/header';
+import { Footer } from './componentes/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
-  title = 'MiAppAngular';
+  title = 'Mortal Kombat';
 }
